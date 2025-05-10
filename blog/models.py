f50f5b11f3a4a87d8592e87e3d2a51aa27f235de
Mikael Malmsten <miksu.malmsten@gmail.com>
2025-05-10 23:45:58 +1000

    No editing functionality required

--- blog/models.py
+++ blog/models.py
@@ -6,11 +6,12 @@ class BlogPost(models.Model):
     content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
-    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"\'{self.title}\' by {self.author}"
 
 class Comment(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(BlogPost, on_delete=models.CASCADE)
     content = models.TextField(max_length=600)
     created = models.DateTimeField(auto_now_add=True)
-    modified = models.DateTimeField(auto_now=True)
