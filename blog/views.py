f7618a73cc1294631c5587aed6d2e3718aa7566e
Mikael Malmsten <miksu.malmsten@gmail.com>
2025-05-11 22:41:48 +1000

    Login working

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
 from .models import BlogPost, Comment
+from django.contrib.auth import logout, login, authenticate
 
 # Create your views here.
 def index(request):
     posts = BlogPost.objects.all().order_by('-created')
     return render(request, 'blog/index.html', {'posts' : posts})
+
+def logout_view(request):
+    logout(request)
+    return redirect('/')
+
+def login_user(request):
+    if request.method == 'POST':
+        print(request.POST)
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            return render(request, 'blog/login.html', {'error' : 'Invalid username or password'})
+    return render(request, 'blog/login.html')
